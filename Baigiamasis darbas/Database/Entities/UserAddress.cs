16370f6e4283ebe6a1aaa76ca3af53bca9a8a816
BimBaMas <dagilis@gmail.com>
2023-01-12 13:21:08 +0200

    Created Entities

---
+++ Baigiamasis darbas/Database/Entities/UserAddress.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Baigiamasis_darbas.Database.Entities
+{
+    public class UserAddress
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [ForeignKey("UserInfo")]
+        public int UserInfoId { get; set; }
+        public string Town { get; set; }
+        public string Street { get; set; }
+        public string HouseNo { get; set; }
+        public string? FlatNo { get; set; }
+    }
+}
---
+++ Baigiamasis darbas/Database/Entities/UserInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Baigiamasis_darbas.Database.Entities
+{
+    public class UserInfo
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PersonalId { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public byte[] Avatar { get; set; }        
+        [ForeignKey("UserAddress")]
+        public int Address { get; set; }
+    }
+}
